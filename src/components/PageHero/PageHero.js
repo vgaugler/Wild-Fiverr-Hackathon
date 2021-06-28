6575e3a2e800ecb1458bb3a868283df023cf5cfa
Victor Gaugler <victor.gaugler@hotmail.fr>
2021-06-28 14:29:57 +0200

    carte mentor firebase

--- src/components/PageHero/PageHero.js
+++ src/components/PageHero/PageHero.js
@@ -1,12 +1,10 @@
-import React from "react";
-import "./PageHero.css";
+import React from 'react';
+import './PageHero.css';
 
 const PageHero = React.memo(function PageHero({ item, name }) {
   return (
-    <div className="heading-center">
-      <h3>
-        {name} / {item} Items
-      </h3>
+    <div className='heading-center'>
+      <h3>{/* {name} / {item} Items */}3 mentor</h3>
     </div>
   );
 });
