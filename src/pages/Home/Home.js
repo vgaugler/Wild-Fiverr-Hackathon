a7fc967621ab9da6896e279f172a126d9d37bad8
Victor Gaugler <victor.gaugler@hotmail.fr>
2021-06-28 12:38:46 +0200

    first

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,21 +1,24 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./Home.css";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Home.css';
 
 export default function Home() {
   return (
-    <div className="info">
-      <div className="content">
-        <div className="head">
-          <h1>Ready for new stuff</h1>
-          <p>Buy new stock at reasonable cost</p>
+    <div className='info'>
+      <div className='content'>
+        <div className='head'>
+          <h1>
+            Ready to find your personal{' '}
+            <span style={{ color: 'green' }}> Mentor </span>{' '}
+          </h1>
+          <p>Fiverr Senpai is for you</p>
 
-          <Link to="/products">
+          <Link to='/products'>
             <button>Get Started</button>
           </Link>
         </div>
       </div>
-      <div className="pic"></div>
+      <div className='pic'></div>
     </div>
   );
 }
