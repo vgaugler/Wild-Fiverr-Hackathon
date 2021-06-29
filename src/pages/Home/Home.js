7b6d17dafb75546ca07ddf0a04691e44bd27cb15
Thomas Allen <71492408+teasmade@users.noreply.github.com>
2021-06-29 16:18:38 +0200

    style accordion, update text home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -11,6 +11,7 @@ import {
   AccordionItemHeading,
   AccordionItemButton,
   AccordionItemPanel,
+  AccordionItemState,
 } from 'react-accessible-accordion';
 
 // import Swiper core and required modules
@@ -28,21 +29,42 @@ SwiperCore.use([Navigation, A11y, EffectFade, Autoplay]);
 const items = [
   {
     uuid: 1,
-    heading: 'HEADING 1 HEADING 1 HEADING 1',
-    content:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias sit natus corporis laudantium? Voluptatibus sunt, eveniet rerum vero ut porro sed, ipsum culpa optio at quidem numquam',
+    heading: 'Get help finding your ideal niche',
+    content: (
+      <>
+        <h3>You're amazing at that thing you do... and we know you want to share it</h3>
+        <div>
+          Your mentor will help you identify gaps in the market, teach you how to adapt your gig
+          offers and guide you in amplifying the reach of your skillset.
+        </div>
+      </>
+    ),
   },
   {
     uuid: 2,
-    heading: 'HEADING 2',
-    content:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias sit natus corporis laudantium? Voluptatibus sunt, eveniet rerum vero ut porro sed, ipsum culpa optio at quidem numquam',
+    heading: 'Learn how to precisely pitch your gigs',
+    content: (
+      <>
+        <h3>Enabling your focus on your talents is the bottom line of all our efforts</h3>
+        <div>
+          The start is always a difficult time. With seasoned advice on how to interact and
+          communicate with buyers, you'll break into the market that much quicker.
+        </div>
+      </>
+    ),
   },
   {
     uuid: 3,
-    heading: 'HEADING 3',
-    content:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias sit natus corporis laudantium? Voluptatibus sunt, eveniet rerum vero ut porro sed, ipsum culpa optio at quidem numquam',
+    heading: 'Build relationships for a profitable future',
+    content: (
+      <>
+        <h3>The Community that Knows Itself, Grows Itself</h3>
+        <div>
+          This is so much more than chasing the next gig ; we're building the Future of Work, and
+          solidarity in our community of sellers is a core part of this.
+        </div>
+      </>
+    ),
   },
 ];
 
@@ -64,25 +86,28 @@ export default function Home() {
               <button>Get Started</button>
             </Link>
           </div>
+          <div className='concept'>
+            <Accordion className='accordion' allowZeroExpanded>
+              {items.map((item) => (
+                <AccordionItem
+                  activeClassName='accordion__active'
+                  className='accordion__item'
+                  key={item.uuid}
+                >
+                  <AccordionItemHeading>
+                    <AccordionItemButton className='accordion__button'>
+                      {item.heading}
+                    </AccordionItemButton>
+                  </AccordionItemHeading>
+                  <AccordionItemPanel className='accordion__panel'>
+                    {item.content}
+                  </AccordionItemPanel>
+                </AccordionItem>
+              ))}
+            </Accordion>
+          </div>
+        </div>
 
-          <Link to='/products'>
-            <button>Get Started</button>
-          </Link>
-        </div>
-        <div className='concept'>
-          <Accordion className='accordion' allowZeroExpanded>
-            {items.map((item) => (
-              <AccordionItem className='accordion__item' key={item.uuid}>
-                <AccordionItemHeading>
-                  <AccordionItemButton className='accordion__button'>
-                    {item.heading}
-                  </AccordionItemButton>
-                </AccordionItemHeading>
-                <AccordionItemPanel className='accordion__panel'>{item.content}</AccordionItemPanel>
-              </AccordionItem>
-            ))}
-          </Accordion>
-        </div>
         <Swiper
           effect='fade'
           autoplay
