63bf4242e781e383a541247f3825b4d5a49d2d77
Paul Bouhours <bouhours.paul@gmail.com>
2021-06-29 16:24:11 +0200

    creat bar search with context

--- src/pages/Products/ProductList.js
+++ src/pages/Products/ProductList.js
@@ -1,9 +1,20 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import firebase from '../../utils/firebaseConfig';
 import MentorCard from '../../components/MentorCard/MentorCard';
+import './ProductList.css';
+import { mentorContext } from '../../context/MentorContext';
 const ProductList = () => {
-  const [mentorList, setMentorList] = useState([]);
+  const {mentorList, setMentorList} = useContext(mentorContext)
+  console.log(mentorList)
+
+  const [search, setSearch] = useState('');
+  const [filtredMentor, setFiltredMentor] = useState([]);
+
+  const handleChange = (event) => {
+    setSearch(event.target.value);
+  };
+
 
   useEffect(() => {
     const mentor = firebase.database().ref('user');
@@ -15,12 +26,42 @@ const ProductList = () => {
         list.push({ id, ...previousList[id] });
       }
       setMentorList(list);
-      console.log(mentorList);
+      setFiltredMentor(list)
+      console.log(mentorList)
+      console.log(filtredMentor)
+      
     });
   }, []);
+
+  useEffect(() => {
+    let temp = mentorList.filter((el) => search && el.activity.toLowerCase().includes(search))
+    console.log(temp)
+    setFiltredMentor(temp)
+    if(!temp.length){
+      setFiltredMentor(mentorList)
+    }
+  }, [search])
+
   return (
     <>
-      <MentorCard mentorList={mentorList} />
+    <div>
+      <form>
+        <fieldset className="barSearch">
+          <label htmlFor="content">
+            <br />
+            <input
+              type="texte"
+              id="content"
+              className="searchMentor"
+              placeholder="  recherche"
+              onChange={(event) => handleChange(event)}
+              value={search}
+            />
+          </label>
+        </fieldset>
+      </form>
+    </div>
+      <MentorCard mentorList={filtredMentor} />
     </>
   );
 };
