e99d7085b12c8ce8e0df6ca2c81ba5a67e66ad93
Victor Gaugler <victor.gaugler@hotmail.fr>
2021-06-29 17:41:58 +0200

    css jumbotron, sticky nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { lazy, Suspense } from 'react';
+import React, { lazy, Suspense, useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Spinner from './components/Spinner/Spinner';
 import SingleProduct from './pages/SingleProduct/SingleProduct';
 import MentorProvider from './context/MentorContext';
-
+import { useWindowScroll } from 'react-use';
 const Cart = lazy(() => import('./pages/CartContent/CartItems'));
 const Homepage = lazy(() => import('./pages/Home/Home'));
 const Errorpage = lazy(() => import('./pages/Error/Error'));
@@ -14,24 +14,30 @@ const ProductDetails = lazy(() =>
 );
 
 export default function App() {
+  const { y: pageYOffset } = useWindowScroll();
+  const [visible, setVisiblity] = useState(false);
+
+  useEffect(() => {
+    if (pageYOffset > 60) {
+      setVisiblity(true);
+    } else {
+      setVisiblity(false);
+    }
+  }, [pageYOffset]);
   return (
     <Router>
-      
-        <Navbar />
-        <Suspense fallback={<Spinner />}>
+      <Navbar visible={visible} />
+      <Suspense fallback={<Spinner />}>
         <MentorProvider>
           <Switch>
-          
             <Route exact path='/' component={Homepage}></Route>
             <Route exact path='/cart' component={Cart}></Route>
             <Route exact path='/products' component={Productlist}></Route>
             <Route exact path='/products/:id' component={SingleProduct} />
             <Route exact path='*' component={Errorpage}></Route>
-            
           </Switch>
-          </MentorProvider>
-        </Suspense>
-      
+        </MentorProvider>
+      </Suspense>
     </Router>
   );
 }
--- src/pages/Products/ProductList.js
+++ src/pages/Products/ProductList.js
@@ -5,8 +5,8 @@ import MentorCard from '../../components/MentorCard/MentorCard';
 import './ProductList.css';
 import { mentorContext } from '../../context/MentorContext';
 const ProductList = () => {
-  const {mentorList, setMentorList} = useContext(mentorContext)
-  console.log(mentorList)
+  const { mentorList, setMentorList } = useContext(mentorContext);
+  console.log(mentorList);
 
   const [search, setSearch] = useState('');
   const [filtredMentor, setFiltredMentor] = useState([]);
@@ -14,7 +14,6 @@ const ProductList = () => {
   const handleChange = (event) => {
     setSearch(event.target.value);
   };
-
 
   useEffect(() => {
     const mentor = firebase.database().ref('user');
@@ -26,41 +25,42 @@ const ProductList = () => {
         list.push({ id, ...previousList[id] });
       }
       setMentorList(list);
-      setFiltredMentor(list)
-      console.log(mentorList)
-      console.log(filtredMentor)
-      
+      setFiltredMentor(list);
+      console.log(mentorList);
+      console.log(filtredMentor);
     });
   }, []);
 
   useEffect(() => {
-    let temp = mentorList.filter((el) => search && el.activity.toLowerCase().includes(search))
-    console.log(temp)
-    setFiltredMentor(temp)
-    if(!temp.length){
-      setFiltredMentor(mentorList)
+    let temp = mentorList.filter(
+      (el) => search && el.activity.toLowerCase().includes(search)
+    );
+    console.log(temp);
+    setFiltredMentor(temp);
+    if (!temp.length) {
+      setFiltredMentor(mentorList);
     }
-  }, [search])
+  }, [search]);
 
   return (
     <>
-    <div>
-      <form>
-        <fieldset className="barSearch">
-          <label htmlFor="content">
-            <br />
-            <input
-              type="texte"
-              id="content"
-              className="searchMentor"
-              placeholder="  recherche"
-              onChange={(event) => handleChange(event)}
-              value={search}
-            />
-          </label>
-        </fieldset>
-      </form>
-    </div>
+      <div style={{ marginTop: '80px' }}>
+        <form>
+          <fieldset className='barSearch'>
+            <label htmlFor='content'>
+              <br />
+              <input
+                type='texte'
+                id='content'
+                className='searchMentor'
+                placeholder='  recherche'
+                onChange={(event) => handleChange(event)}
+                value={search}
+              />
+            </label>
+          </fieldset>
+        </form>
+      </div>
       <MentorCard mentorList={filtredMentor} />
     </>
   );
